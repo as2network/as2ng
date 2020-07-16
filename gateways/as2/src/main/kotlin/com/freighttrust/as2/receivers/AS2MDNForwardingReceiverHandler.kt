536cea3a3980bd2e17d098c9f3f5e95648c78677
Aldo <aldo@41north.dev>
2020-07-16 08:42:43 +0200

    feat: converted AS2MDNForwardingReceiverHandler more to kotlin

--- gateways/as2/src/main/kotlin/com/freighttrust/as2/receivers/AS2MDNForwardingReceiverHandler.kt
+++ gateways/as2/src/main/kotlin/com/freighttrust/as2/receivers/AS2MDNForwardingReceiverHandler.kt
@@ -58,7 +58,6 @@ import com.helger.as2lib.util.AS2ResourceHelper
 import com.helger.as2lib.util.http.AS2HttpResponseHandlerSocket
 import com.helger.as2lib.util.http.AS2InputStreamProviderSocket
 import com.helger.as2lib.util.http.HTTPHelper
-import com.helger.as2lib.util.http.IAS2HttpResponseHandler
 import com.helger.commons.http.CHttp
 import com.helger.commons.http.CHttpHeader
 import com.helger.commons.io.stream.StreamHelper
@@ -80,18 +79,20 @@ class AS2MDNForwardingReceiverHandler(
   private val okHttpClient: OkHttpClient
 ) : AbstractReceiverHandler() {
 
-  private val logger = LoggerFactory.getLogger(AS2MDNForwardingReceiverHandler::class.java)
-
+  private val session = receiverModule.session
+  private val partnershipFactory = session.partnershipFactory
+  private val certificateFactory = session.certificateFactory
   private val micMatchingHandler: IMICMatchingHandler = LoggingMICMatchingHandler()
+
+  private val logger = LoggerFactory.getLogger(AS2MDNForwardingReceiverHandler::class.java)
 
   override fun handle(aOwner: AbstractActiveNetModule, socket: Socket) {
 
     val clientInfo = getClientInfo(socket)
-      .also { info -> if (logger.isInfoEnabled) logger.info("Incoming connection $info") }
+      .also { info -> logger.info("Incoming connection $info") }
 
     val message = AS2Message()
-    val bQuoteHeaderValues = receiverModule.isQuoteHeaderValues
-    val responseHandler: IAS2HttpResponseHandler = AS2HttpResponseHandlerSocket(socket, bQuoteHeaderValues)
+    val responseHandler = AS2HttpResponseHandlerSocket(socket, receiverModule.isQuoteHeaderValues)
 
     // Read in the message request, headers, and data
     try {
@@ -109,7 +110,7 @@ class AS2MDNForwardingReceiverHandler(
         // Asynch MDN 2007-03-12
         // check if the requested URL is defined in attribute "as2_receipt_option"
         // in one of partnerships, if yes, then process incoming AsyncMDN
-        if (logger.isInfoEnabled) logger.info("incoming connection for receiving AsyncMDN" + " [" + clientInfo + "]" + message.loggingText)
+        logger.info("incoming connection for receiving AsyncMDN [$clientInfo]${message.loggingText}")
 
         val receivedContentType = AS2HttpHelper.getCleanContentType(message.getHeader(CHttpHeader.CONTENT_TYPE))
         val receivedPart = MimeBodyPart(AS2HttpHelper.getAsInternetHeaders(message.headers()), data)
@@ -122,164 +123,133 @@ class AS2MDNForwardingReceiverHandler(
         receivedPart.setHeader(CHttpHeader.CONTENT_TYPE, receivedContentType)
         message.data = receivedPart
 
-        receiveMDN(message, data, responseHandler, resourceHelper)
+        // Process MDN
+        try {
+          // Create a MessageMDN and copy HTTP headers
+          val mdn = AS2MessageMDN(message)
+
+          // copy headers from msg to MDN from msg
+          mdn.headers().setAllHeaders(message.headers())
+          val part = MimeBodyPart(AS2HttpHelper.getAsInternetHeaders(mdn.headers()), data)
+          message.mdn!!.data = part
+
+          mdn.partnership()
+            .apply {
+
+              // get the MDN partnership info
+              senderAS2ID = mdn.getHeader(CHttpHeader.AS2_FROM)
+              receiverAS2ID = mdn.getHeader(CHttpHeader.AS2_TO)
+            }
+
+          // Update the partnership
+          partnershipFactory.updatePartnership(mdn, false)
+
+          val senderCertificate = certificateFactory.getCertificate(mdn, ECertificatePartnershipType.SENDER)
+
+          val useCertificateInBodyPart = message
+            .partnership()
+            .verifyUseCertificateInBodyPart
+            // User per partnership or default to global value
+            .let {
+              if (it.isDefined)
+                it.asBooleanValue
+              else
+                session.isCryptoVerifyUseCertificateInBodyPart
+            }
+
+          AS2Helper.parseMDN(
+            message,
+            senderCertificate,
+            useCertificateInBodyPart,
+            verificationCertificateConsumer,
+            resourceHelper
+          )
+
+          // in order to name & save the mdn with the original AS2-From + AS2-To +
+          // Message id.,
+          // the 3 msg attributes have to be reset before calling MDNFileModule
+
+          message.partnership()
+            .apply {
+              senderAS2ID = mdn.getHeader(CHttpHeader.AS2_TO)
+              receiverAS2ID = mdn.getHeader(CHttpHeader.AS2_FROM)
+            }
+
+          partnershipFactory.updatePartnership(message, false)
+          message.messageID = mdn.attrs().getAsString(AS2MessageMDN.MDNA_ORIG_MESSAGEID)
+
+          // check if the mic (message integrity check) is correct
+          if (checkAsyncMDN(message))
+            HTTPHelper.sendSimpleHTTPResponse(responseHandler, CHttp.HTTP_OK)
+          else
+            HTTPHelper.sendSimpleHTTPResponse(responseHandler, CHttp.HTTP_NOT_FOUND)
+
+          val disposition = message.mdn!!.attrs().getAsString(AS2MessageMDN.MDNA_DISPOSITION)
+
+          try {
+            DispositionType.createFromString(disposition).validate()
+          } catch (ex: AS2DispositionException) {
+            ex.text = message.mdn!!.text
+            if (ex.disposition.isWarning) {
+              // Warning
+              ex.setSourceMsg(message).terminate()
+            } else {
+              // Error
+              throw ex
+            }
+          }
+
+          // TODO: Review here how to send properly the body
+          val mediaType = part.contentType.split("\r").first().toMediaType()
+          val body = data.toRequestBody(mediaType)
+
+          // store first
+
+          val bodyRecord = fileRepository.insert(mdn.messageID!!, part.inputStream, body.contentLength())
+          val mdnRecord = mdn.toAs2MdnRecord(bodyRecord)
+          as2MdnRepository.insert(mdnRecord)
+
+          // Forward message to origin
+          val url = message.partnership().aS2MDNTo!!
+
+          val request = Request.Builder()
+            .url(url)
+            .apply {
+              message.headers()
+                .allHeaders
+                // need to make sure not to send the original content type which has boundary info encoded in it
+                .filter { h -> h.key != CHttpHeader.CONTENT_TYPE }
+                .forEach { h -> header(h.key, h.value.first!!) }
+              header(CHttpHeader.RECEIPT_DELIVERY_OPTION, url)
+            }
+            .post(body)
+            .build()
+
+          okHttpClient
+            .newCall(request)
+            .execute()
+            .use { response ->
+
+              when {
+                response.isSuccessful -> {
+                }
+                response.isNotSuccessful -> {
+                }
+              }
+            }
+        } catch (ex: IOException) {
+          HTTPHelper.sendSimpleHTTPResponse(responseHandler, CHttp.HTTP_BAD_REQUEST)
+          throw ex
+        } catch (ex: Exception) {
+          // TODO improve exception logging as important info was being obscured
+          ex.printStackTrace()
+          HTTPHelper.sendSimpleHTTPResponse(responseHandler, CHttp.HTTP_BAD_REQUEST)
+          throw WrappedAS2Exception.wrap(ex).setSourceMsg(message)
+        }
       }
     } catch (ex: Exception) {
       val ne = AS2NetException(socket.inetAddress, socket.port, ex)
       ne.terminate()
-    }
-  }
-
-  /**
-   * method for receiving and processing Async MDN sent from receiver.
-   *
-   * @param message
-   * The MDN message
-   * @param data
-   * The MDN content
-   * @param responseHandler
-   * The HTTP response handler for setting the correct HTTP response code
-   * @param resourceHelper
-   * Resource helper
-   * @throws AS2Exception
-   * In case of error
-   * @throws IOException
-   * In case of IO error
-   */
-  @Throws(AS2Exception::class, IOException::class)
-  private fun receiveMDN(
-    message: AS2Message,
-    data: ByteArray,
-    responseHandler: IAS2HttpResponseHandler,
-    resourceHelper: AS2ResourceHelper
-  ) {
-    try {
-      // Create a MessageMDN and copy HTTP headers
-      val mdn = AS2MessageMDN(message)
-
-      // copy headers from msg to MDN from msg
-      mdn.headers().setAllHeaders(message.headers())
-      val part = MimeBodyPart(AS2HttpHelper.getAsInternetHeaders(mdn.headers()), data)
-      message.mdn!!.data = part
-
-      mdn.partnership()
-        .apply {
-
-          // get the MDN partnership info
-          senderAS2ID = mdn.getHeader(CHttpHeader.AS2_FROM)
-          receiverAS2ID = mdn.getHeader(CHttpHeader.AS2_TO)
-
-          // TODO: These are null as the message is Async
-          // Set the appropriate keystore aliases
-          // senderX509Alias = message.partnership().receiverX509Alias
-          // receiverX509Alias = message.partnership().senderX509Alias
-        }
-
-      // Update the partnership
-      receiverModule.session.partnershipFactory.updatePartnership(mdn, false)
-
-      val certificateFactory = receiverModule.session.certificateFactory
-      val senderCertificate = certificateFactory.getCertificate(mdn, ECertificatePartnershipType.SENDER)
-
-      val useCertificateInBodyPart = message
-        .partnership()
-        .verifyUseCertificateInBodyPart
-        // User per partnership or default to global value
-        .let {
-          if (it.isDefined)
-            it.asBooleanValue
-          else
-            receiverModule.session.isCryptoVerifyUseCertificateInBodyPart
-        }
-
-      AS2Helper.parseMDN(
-        message,
-        senderCertificate,
-        useCertificateInBodyPart,
-        verificationCertificateConsumer,
-        resourceHelper
-      )
-
-      // in order to name & save the mdn with the original AS2-From + AS2-To +
-      // Message id.,
-      // the 3 msg attributes have to be reset before calling MDNFileModule
-
-      message.partnership()
-        .apply {
-          senderAS2ID = mdn.getHeader(CHttpHeader.AS2_TO)
-          receiverAS2ID = mdn.getHeader(CHttpHeader.AS2_FROM)
-        }
-
-      receiverModule.session.partnershipFactory.updatePartnership(message, false)
-      message.messageID = mdn.attrs().getAsString(AS2MessageMDN.MDNA_ORIG_MESSAGEID)
-
-      // check if the mic (message integrity check) is correct
-      if (checkAsyncMDN(message))
-        HTTPHelper.sendSimpleHTTPResponse(responseHandler, CHttp.HTTP_OK)
-      else
-        HTTPHelper.sendSimpleHTTPResponse(responseHandler, CHttp.HTTP_NOT_FOUND)
-
-      val disposition = message.mdn!!.attrs().getAsString(AS2MessageMDN.MDNA_DISPOSITION)
-
-      try {
-        DispositionType.createFromString(disposition).validate()
-      } catch (ex: AS2DispositionException) {
-        ex.text = message.mdn!!.text
-        if (ex.disposition.isWarning) {
-          // Warning
-          ex.setSourceMsg(message).terminate()
-        } else {
-          // Error
-          throw ex
-        }
-      }
-
-      // TODO: Review here how to send properly the body
-      val mediaType = part.contentType.split("\r").first().toMediaType()
-      val body = data.toRequestBody(mediaType)
-
-      // store first
-
-      val bodyRecord = fileRepository.insert(mdn.messageID!!, part.inputStream, body.contentLength())
-      val mdnRecord = mdn.toAs2MdnRecord(bodyRecord)
-      as2MdnRepository.insert(mdnRecord)
-
-      // Forward message to origin
-      val url = message.partnership().aS2MDNTo!!
-
-      val request = Request.Builder()
-        .url(url)
-        .apply {
-          message.headers()
-            .allHeaders
-            // need to make sure not to send the original content type which has boundary info encoded in it
-            .filter { h -> h.key != CHttpHeader.CONTENT_TYPE }
-            .forEach { h -> header(h.key, h.value.first!!) }
-          header(CHttpHeader.RECEIPT_DELIVERY_OPTION, url)
-        }
-        .post(body)
-        .build()
-
-      okHttpClient
-        .newCall(request)
-        .execute()
-        .use { response ->
-
-          when {
-            response.isSuccessful -> {
-            }
-            response.isNotSuccessful -> {
-            }
-          }
-        }
-    } catch (ex: IOException) {
-      HTTPHelper.sendSimpleHTTPResponse(responseHandler, CHttp.HTTP_BAD_REQUEST)
-      throw ex
-    } catch (ex: Exception) {
-      // TODO improve exception logging as important info was being obscured
-      ex.printStackTrace()
-      HTTPHelper.sendSimpleHTTPResponse(responseHandler, CHttp.HTTP_BAD_REQUEST)
-      throw WrappedAS2Exception.wrap(ex).setSourceMsg(message)
     }
   }
 
@@ -310,7 +280,7 @@ class AS2MDNForwardingReceiverHandler(
 
       val dispositionStr = message.mdn!!.attrs().getAsString(AS2MessageMDN.MDNA_DISPOSITION)
 
-      if (logger.isInfoEnabled) logger.info("received MDN [" + dispositionStr + "]" + message.loggingText)
+      logger.info("received MDN [" + dispositionStr + "]" + message.loggingText)
 
       if (originalMIC == null || returnedMIC == null || returnedMIC != originalMIC) {
         micMatchingHandler.onMICMismatch(message, originalMICStr, returnedMicStr)
